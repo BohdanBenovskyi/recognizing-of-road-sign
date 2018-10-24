ca911a5200b8aecea984a8d9fd3ff6c6c69d9206
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-10-24 21:40:52 +0300

    Add ArticlesController.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,26 @@
 class ArticlesController < ApplicationController
   def new
-
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+  end
 
+  def show
+    @article = Article.find(params[:id])
   end
 
   def destroy
-
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to root_path
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :preview, :content, :thumbnail);
+    end
 end
