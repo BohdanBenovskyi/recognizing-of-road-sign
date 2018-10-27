3278e4118682bd51d76994d4e9a22b48416f13fe
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-10-27 17:29:54 +0300

    Add edit action for ArticlesController.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,21 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated"
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = "Article was not updated"
+      render 'edit'
+    end
+  end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
