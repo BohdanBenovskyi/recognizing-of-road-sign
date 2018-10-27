503ea0261d4bc8db0e4e72d779ed2846139d3246
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-10-27 22:39:21 +0300

    Change association for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :resumes
+  has_many :resumes, dependent: :destroy
 end
