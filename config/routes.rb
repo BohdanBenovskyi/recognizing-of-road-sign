173fbe8f47e32aae84ed78a36d74ff414b617f22
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-10-27 19:21:29 +0300

    Add resumes to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'resumes/index'
+  get 'resumes/new'
+  get 'resumes/create'
+  get 'resumes/destroy'
   devise_for :users
   get 'welcome/index'
   root 'welcome#index'
 
   resources :articles, only: [:new, :show, :create, :edit, :update, :destroy]
+  resources :resumes, only: [:index, :new, :create, :destroy]
 end
